c1cef52acdbb459a185c61fbae49e7f12187eca5
Azret Zhusuev <a.zhusuev@quickpay.kg>
2023-03-01 20:34:44 +0600

    create 2 api for get clients and for create client

---
+++ app/Http/Controllers/API/ClientController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Client;
+//use Dotenv\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['clients' => Client::all()]);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'full_name' => 'required|string:200',
+            'phone_number' => 'required|string',
+            'email' => 'required|string'
+        ]);
+
+        if ($validation->fails()){
+            return response()->json(['status' => 'error', 'message' => $validation->getMessageBag()], 400);
+        } else {
+            return response()->json(['status' => 'success', 'clients' => Client::create($request->all())], 200);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\ClientController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('clients', [ClientController::class, 'index'])->name('clients.api.index');
+Route::post('clients/store', [ClientController::class, 'store'])->name('clients.api.store');
