068883eacae5ab2c9bf1c109633a7f4f2ef91385
eardakbekov1 <eardakbekov@gmail.com>
2023-03-02 18:59:54 +0600

    api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/clients', [\App\Http\Controllers\API\ClientController::class, 'index'])->name('clients.api.index');
+Route::post('clients/store', [\App\Http\Controllers\API\ClientController::class, 'store'])->name('clients.api.store');
